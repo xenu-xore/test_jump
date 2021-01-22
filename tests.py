caefa012e6e63c4aad3d8e52cb9b2e6432e2c116
xenu-xore <52954685+xenu-xore@users.noreply.github.com>
2021-01-22 11:45:32 +0300

    init

--- tests.py
+++ tests.py
@@ -2,51 +2,54 @@ import unittest
 import os
 from main import ProgramSearch
 import shutil
+import csv
 
-# foo = ProgramSearch('data')
-# print(foo.reade_csv('RUS'))
-# foo.directory = 'data'
-# soo = ProgramSearch(foo.directory)
-# print(soo.reade_csv('RUS'))
 
 class TestProgramSearchMethods(unittest.TestCase):
-    """ТЕвсты для функции"""
+    """Тесты для функции"""
+    myData_test = [['2013-02-08', '67.7142', '68.4014', '66.8928', '67.8542', '158168416', 'RUS'],
+                   ['2013-02-11', '1.50', '2.50', '3.50', '4.50', '5', 'TEST'],
+                   ['2013-02-12', '68.5014', '68.9114', '66.8205', '66.8428', '151829363', 'PL']]
+
+    names_test = ['date', 'open', 'high', 'low', 'close', 'volume', 'Name']
+
     def test_writer_csv(self):
-        for i in range(5):
-            with open(i+".csv" )
-        search
-        path = os.getcwd() + "/test/dir1/dir2/"
+        path = os.getcwd() + "/test/dir1/dir2/dir3/"
 
         try:
             os.makedirs(path)
+
+            dir_name = []
+            for dirpath, dirnames, filenames in os.walk("test"):
+                for dirname in dirnames:
+                    dir_name.append(os.path.join(dirpath, dirname))
+            print(dir_name)
+            for i in range(1):
+                for dirs in dir_name:
+                    with open(dirs + "-" + str(i) + ".csv", "w+") as f:
+                        csv.writer(f)
+                        ps = ProgramSearch("test")
+                        ps.myData = self.myData_test
+                        ps.names = self.names_test
+                        ps.generate_dict()
+                        ps.writer_csv()
         except OSError:
-            print("Не создалась %s" % path)
+
+            print("Создать директорию %s не удалось" % path)
         else:
-            print("Создано %s" % path)
+            print("Успешно создана директория %s" % path)
 
-        ps = ProgramSearch("test")
-        ps.writer_csv()
+    def test_reade_csv(self):
+        path_del = os.getcwd() + "/test/"
 
-        # path_del = os.getcwd() + "/test/"
-        #
-        # try:
-        #     shutil.rmtree(path_del)
-        # except OSError:
-        #     print("Удалить директорию %s не удалось" % path_del)
-        # else:
-        #     print("Успешно удалена директория %s" % path_del)
-
-
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
-
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # Проверим, что s.split не работает, если разделитель - не строка
-        with self.assertRaises(TypeError):
-            s.split(2)
+        try:
+            ps = ProgramSearch("test")
+            if self.assertTrue(ps.reade_csv("TEST")["open"] == 1.5):
+                shutil.rmtree(path_del)
+        except OSError:
+            print("Удалить директорию %s не удалось" % path_del)
+        else:
+            print("Успешно удалена директория %s" % path_del)
 
 
 if __name__ == '__main__':
